2e8c59430882cdfd0ac758ea250c002d3eb2b684
maaush <mauribenitez95@gmail.com>
2020-04-04 00:31:02 -0300

    Finish homework zz

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Desarrollar una aplicacion en c# para que dado un número de entrada calcule su
+             * correspondiente tabla de multiplicar
+             */ 
+            
+            Console.WriteLine("Input an Integer: ");
+            int number = Convert.ToInt32(Console.Read());
+
+            Console.WriteLine("Tabla de multiplicar: ");
+            for (int j = 0; j < number; j++)
+            {
+                Console.WriteLine(j * number);
+            }
+        }
+    }
+}
